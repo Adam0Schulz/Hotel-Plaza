2fb3c7f052116b2b0565eb30204185fafacc05bb
babakdat21i <90601226+babakdat21i@users.noreply.github.com>
2021-11-21 14:14:04 +0000

    Implemented Guest class

--- src/Guest.java
+++ src/Guest.java
@@ -1,3 +1,39 @@
 public class Guest implements GuestInter {
+   
+    private String name; 
+    private String address; 
+    private int phoneNum; 
+  
+    public Guest (String name, String address, int phoneNum) {
+        this.setName(name);
+        this.setAddress(address);
+        this.setPhoneNum(phoneNum);
+    }
 
+    public String getName(){
+        return this.name;   
+    }
+        
+   
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+   
+    public void setAddress(String address){
+        this.address=address;
+    }
+
+   public int getPhoneNum(){
+        return this.phoneNum;
+    }
+
+   public void setPhoneNum(int phoneNum){
+        this.phoneNum=phoneNum;
+
+    }
 }
